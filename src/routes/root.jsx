7482605eb751d9334349c15636a2c2d8f63bf57d
Andrew Johnson <andrew.k.johnson228@gmail.com>
2023-07-12 19:41:20 -0700

    added useNavigation and navigation.state to my root.jsx

--- src/routes/root.jsx
+++ src/routes/root.jsx
@@ -1,13 +1,21 @@
-import { Outlet, Link, useLoaderData, Form, redirect } from "react-router-dom";
+import {
+  Outlet,
+  NavLink,
+  useLoaderData,
+  Form,
+  redirect,
+  useNavigation,
+} from "react-router-dom";
 import { getContacts, createContact } from "../contacts";
 
 export async function action() {
-   const contact = await createContact();
-   return redirect(`/contacts/${contact.id}/edit`);
- }
+  const contact = await createContact();
+  return redirect(`/contacts/${contact.id}/edit`);
+}
 
 export default function Root() {
   const { contacts } = useLoaderData();
+  const navigation = useNavigation();
   return (
     <>
       <div id="sidebar">
@@ -34,7 +42,15 @@ export default function Root() {
             <ul>
               {contacts.map((contact) => (
                 <li key={contact.id}>
-                  <Link to={`contacts/${contact.id}`}>
+                  <NavLink to={`contacts/${contact.id}`}
+                   className={({ isActive, isPending }) =>
+                   isActive
+                     ? "active"
+                     : isPending
+                     ? "pending"
+                     : ""
+                 }
+               >
                     {contact.first || contact.last ? (
                       <>
                         {contact.first} {contact.last}
@@ -43,7 +59,7 @@ export default function Root() {
                       <i>No Name</i>
                     )}{" "}
                     {contact.favorite && <span>★</span>}
-                  </Link>
+                  </NavLink>
                 </li>
               ))}
             </ul>
@@ -54,7 +70,10 @@ export default function Root() {
           )}
         </nav>
       </div>
-      <div id="detail">
+      <div id="detail"
+         className={
+            navigation.state === "loading" ? "loading" : ""
+       }>
         <Outlet />
       </div>
     </>
